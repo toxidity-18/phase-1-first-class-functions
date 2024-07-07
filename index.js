98c1a7330e9fc6aab6be81b950dfb1c34d943cbd
Samuel <samuel.kamawira@student.moringaschool.com>
2024-07-07 09:44:16 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+function receivesAFunction(callback) {
+    callback();
+  }
+  
+  function returnsANamedFunction() {
+    function namedFunction() {
+    }
+    return namedFunction;
+  }
+  
+  function returnsAnAnonymousFunction() {
+    return function() {
+    }
+  }
+  module.exports = {
+    receivesAFunction,
+    returnsANamedFunction,
+    returnsAnAnonymousFunction
+  };
+  
